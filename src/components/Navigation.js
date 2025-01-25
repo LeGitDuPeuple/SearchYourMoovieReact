90980debc86cda517360ebf300095d0d4e84664d
Sofiane Sahraoui <sofiane.sahraoui17@gmail.com>
2025-01-25 19:53:07 +0100

    commit 1

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+      <div className="navB">
+        <div className="titre">
+            <h1>SearchYourMovie</h1>
+        </div>
+        <div className="btnCherche">
+            <input type="text" />
+        </div>
+      </div>
+    );
+};
+
+export default Navigation;
